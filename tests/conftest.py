cdef650d915fc7986a0ea3fcc45c7cdf4605b802
kowalcj0 <kowalczykjanusz@gmail.com>
2021-03-06 23:42:56 +0000

    Add missing docstrings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Config file for PyTest"""
 import os
 
 from pytest import fixture
@@ -6,17 +7,20 @@ from pytest import fixture
 
 @fixture
 def single_file_multiple_tracks():
+    """Return a path to a cue file with multiple tracks defined in it."""
     cue_file = "ISO-8859_text_with_CRLF_line_terminators.cue"
     return os.path.join("tests", "files", cue_file)
 
 
 @fixture
 def multiple_files_single_track():
+    """Returns a path to a CUE file with single track per multiple source files."""
     cue_file = "ASCII_text.cue"
     return os.path.join("tests", "files", cue_file)
 
 
 @fixture
 def multiple_files_multiple_tracks():
+    """Returns a path to a CUE file with multiple tracks per source file."""
     cue_file = "Non-ISO_extended-ASCII_text_with_CRLF.cue"
     return os.path.join("tests", "files", cue_file)
